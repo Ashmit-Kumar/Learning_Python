6a616f473d380624b36c98b6a550f196d6170b47
Ashmit Kumar <ashmitkumar1020@gmail.com>
2024-06-23 11:12:27 +0530

    Add files

--- 05_importLib.py
+++ 05_importLib.py
@@ -3,9 +3,17 @@ import random
 def get_choices():
     player_Choice=input("Enter your choice(rock ,paper, scissor)")
     option=["Rock","Paper","Scissor"]
-    computer_choice=random.choice(option)
+    computer_choice=random.choice(option).lower()
     choice={"Player":player_Choice,"Computer":computer_choice}
-    return choice
+    # print(choice)
+    win=check_win(player_Choice,computer_choice)
+    return win
+
+def check_win(player,computer):
+    print("you chose "+player+" computer chose "+computer)
+    print()
+    if player==computer:
+        return "It's a tie! "
 
 
 result=get_choices()
---
+++ 06_fString.py
@@ -0,0 +1,19 @@
+import random
+def get_Choice():
+    player_choice=input("Enter your Choice(Rock Paper Scissor)")
+    options=['Rock','Paper','Scissor']
+    computer_choice=random.choice(options)
+    choice={
+        "Player":player_choice,
+        "Computer":computer_choice
+    }   
+    win=get_win(player_choice,computer_choice) 
+    return win
+
+def get_win(player,computer):
+    prnt("Player Choice "+player+" Computer Choice "+computer)
+    if player==computer:
+        return "It's a Tie!😮😮😮"
+    
+result=get_Choice()
+print(result)

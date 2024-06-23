fbb828767b9bfa480d35aafd1f66bcf05e5252e1
Ashmit Kumar <ashmitkumar1020@gmail.com>
2024-06-23 15:15:59 +0530

    update File

---
+++ 09_Strings.py
@@ -0,0 +1,54 @@
+F_name="Roronoa"
+L_name="Zoro"
+print(F_name+" "+L_name) # Concatenation using + operator 
+
+
+#appending the String
+name="Monkey"
+name+=" D."
+name+=" Luffy"
+print(name)
+
+# casting
+age=str(49)
+print(age)
+
+#multi line String
+print("""
+     This is multi Line String 
+      hello
+"""
+)
+print(''''
+     This is multi Line String 
+      hello
+'''
+)
+
+print("ADD".islower())
+s="AAAAAAa"
+ss="Ashmit"
+s1="AA"
+s2="aaaa bbbbbb"
+print(s.upper()) 
+print(s.lower())
+print(s1.islower())
+print(s1.isupper())
+print(s2.title())
+print(s2.startswith("aaa"))
+print(s.startswith("A"))
+print(s.endswith("Aa"))
+print(ss.replace("shm","mhs")) #here you pass the substring you want to change with the new substring 
+# print(ss.replace(0,'k')) error 
+print(ss.replace("As",''))  
+
+s2.split()
+
+# https://www.youtube.com/watch?v=eWRfhZUzrAc&list=PLWKjhJtqVAbnqBxcdjVGgT3uVR10bzTEB
+# 1:16:40
+
+
+
+
+
+

dddc4acd4a740d77419e87275345bb08d98be02d
Ashmit Kumar <ashmitkumar1020@gmail.com>
2024-06-23 14:14:42 +0530

    Update Variables

--- 03_Variables_Data.py
+++ 03_Variables_Data.py
@@ -7,11 +7,26 @@ def get_choices():
 choices=get_choices()
 print(choices)
 
+# name="hello"; print(name)
+name="Hello"
+print(type(name))
+print(isinstance(name,str))
+n=int("20")
+print(isinstance(n,int))
+# n1=int("k")
+# print(isinstance(n1,int))
+# print(5.5//2)  floor divison
+# print(5.5/2)
 
-'''def greeting():
-    return "Hi"
 
-print(greeting())
+'''
+a variable name can be composed of character, number and underscore but it cannot start with an number you cannot use kewords as a variable name
+ex-:
+name1="sdf"
+hiegth=30
+
+you cannot use test! or test% these as variable name also
+
 '''
 # name="John"
 # age=23

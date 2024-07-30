effe2c5b0111f7086ee2d8a5ea2d8e14be0f329c
Ashmit Kumar <ashmitkumar1020@gmail.com>
2024-07-30 20:56:43 +0530

    update FileHandling

--- 13_File_Handling.py
+++ 13_File_Handling.py
@@ -31,5 +31,101 @@ Text/Binary
 rt/rbt(read and write)
 wt/rbt(read and write)
 at/rbt(append and read)
+
+
+#Open a File
+#open("filename",filemode)  
+#by default it open a file in reading mode
+
+Reading content of a file
+There are three types of read function to read content in a python file
+
+1 read()
+2 readline()
+3 readlines()
+
+1) read([n]):to read n bytes from a file if n is missing, read the entire file content
 '''
 
+f=open("abc.txt","r")
+v=f.read() # read whole file
+print(v)
+f.close()
+
+f=open("abc.txt","r")
+v=f.read(10) #reads first 10 character of a file
+print(v)
+f.close()
+
+
+f=open("abc.txt","r")
+v=f.readline() #read first line
+print(v)
+f.close()
+
+f=open("abc.txt","r")
+for i in f:
+    print(i+":")
+f.close()
+
+f=open("abc.txt","r")
+v=f.readlines()
+print(v)
+f.close()
+
+
+f=open("abc.txt","r")
+v=f.read()
+c=0
+for i in v:
+    if i==' ':
+        c+=1
+f.close()
+print(c)
+
+
+f=open('abc.txt','r')
+v=f.read()
+vowels=0
+alpha=0
+alpha2=0
+for i in v:
+    # print(i)
+    # if i in 'a' or i in 'e' or i=='i' or i=='o' or i=='u':
+    #     vowels+=1
+    #if i in ['a','e','i','o','u']
+    if i in 'aeiouAEIOU':
+        vowels+=1
+    if i.isalpha():
+        alpha+=1
+print(vowels)
+print(alpha)
+print(alpha2)
+f.close()
+
+
+f=open('abc.txt','r+t')
+v=f.read()
+while True:
+    if v.find("aa"):
+        print("mil gaya")
+        break
+    else:
+        break
+f.close()
+
+f=open('abcd.txt','w')
+f.write("Hello \n")
+f.write("JJJJJ")
+f.write("La La La \n")
+f.close()
+f=open('abcd.txt','r')
+v=f.read()
+f.close()
+print(v)
+
+
+f=open('abcd.txt','w')
+l=['aaaaaaa','bbbbbbbbbbbbb','cccccc\n','ffffffff']
+f.writelines(l)
+f.close()
